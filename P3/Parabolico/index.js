ca97f2770270a8d0b319c34694bbb04beeb2a22d
l.hernandezn.2018 <l.hernandezn.2018@alumnos.urjc.es>
2023-03-23 09:55:27 +0100

    commit tiro

--- P3/Parabolico/index.js
+++ P3/Parabolico/index.js
@@ -4,18 +4,17 @@ console.log("Ejecutando JS...");
 
 function getRandomInt(min, max) {
     x_coord = Math.floor(Math.random() * (max - min)) + min;
-
 }
 
-obtener_coord = getRandomInt(100,180);
+obtener_coord = getRandomInt(150,280);
 console.log(x_coord);
 
 
 const canvas = document.getElementById("canvas");
 
 //-- Definir el tamaño del canvas
-canvas.width = 200;
-canvas.height = 100;
+canvas.width = 300;
+canvas.height = 150;
 
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
@@ -24,10 +23,10 @@ const ctx = canvas.getContext("2d");
 ctx.beginPath();
     //-- Dibujar un circulo: coordenadas x,y del centro
     //-- Radio, Angulo inicial y angulo final
-    ctx.arc(x_coord, 87, 10, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 3;
-    ctx.fillStyle = 'blue';
+    ctx.arc(x_coord, 137, 10, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'blue';  //contorno
+    ctx.lineWidth = 3;  //grosor del contorno
+    ctx.fillStyle = 'red';  //relleno
 
     //-- Dibujar el trazo
     ctx.stroke()
@@ -40,9 +39,9 @@ ctx.closePath()
 ctx.beginPath();
     //-- Dibujar un circulo: coordenadas x,y del centro
     //-- Radio, Angulo inicial y angulo final
-    ctx.rect(4, 77, 20, 20);
+    ctx.rect(4, 127, 20, 20);
     ctx.strokeStyle = 'green';
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 2;
     ctx.fillStyle = 'green';
 
     //-- Dibujar el trazo
@@ -53,3 +52,10 @@ ctx.beginPath();
     
 ctx.closePath()
 
+angulo.oninput = () => {
+    display1.innerHTML = angulo.value;
+}
+
+velocidad.oninput = () => {
+  display2.innerHTML = velocidad.value;
+}
