4d63b8cb90c8007236717b800ae450f4c828974b
l.hernandezn.2018 <l.hernandezn.2018@alumnos.urjc.es>
2023-03-23 10:29:40 +0100

    commit tiro

--- P3/Parabolico/index.js
+++ P3/Parabolico/index.js
@@ -63,6 +63,9 @@ ctx.beginPath();
 ctx.closePath()
 
 
+lr =20;
+
+
 //Ángulo:
 angulo.onchange = () => {
     display1.innerHTML = angulo.value;
@@ -85,6 +88,72 @@ velocidad.onchange = () => {
 gui.disparo.onclick = () => {
     console.log('dispara')
     crono.start();
+    
+    
+    //-- Funcion principal de animacion
+    function update() 
+    {
+    
+        //-- Algoritmo de animacion:
+        //-- 1) Actualizar posición del  elemento
+        //-- (física del movimiento rectilineo uniforme)
+
+        //-- Condicion de rebote en extremos del canvas
+        if (x1 < 0 || x1 >= (canvas.width - lr) ) {
+            velx1 = -velx1;
+            //lr = lr + 1;  //esto da problemas
+        }
+        if (y1 <=  0 || y1 > canvas.height - lr ) {
+            vely1 = -vely1;
+            //lr = lr - 1;  //esto da problemas
+        }
+
+        //-- Condicion de rebote en extremos del canvas
+        if (x2 < 0 || x2 >= (canvas.width - lr) ) {
+            velx2 = -velx2;
+            //lr = lr + 1;  //esto da problemas
+        }
+        if (y2 <=  0 || y2 > canvas.height - lr ) {
+            vely2 = -vely2;
+            //lr = lr - 1;  //esto da problemas
+        }
+
+        //-- Actualizar la posición
+        x1 = x1 + velx1;
+        y1 = y1 + vely1;
+
+        x2 = x2 + velx2;
+        y2 = y2 + vely2;
+
+        //-- 2) Borrar el canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //-- 3) Dibujar los elementos visibles
+        ctx.beginPath();
+            ctx.arc(x, y, 1, 0, 2 * Math.PI);  //bola de disparo
+
+            //-- Dibujar
+            ctx.fillStyle = 'black';
+
+            //-- Rellenar
+            ctx.fill();
+
+            //-- Dibujar el trazo
+            ctx.stroke()
+        ctx.closePath();
+
+        //-- 4) Volver a ejecutar update cuando toque
+        requestAnimationFrame(update);
+        }
+
+    //-- ¡Que empiece la función!
+    update();
+
+
+
+
+
+
     
 }
 
