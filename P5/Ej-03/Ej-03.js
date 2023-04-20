1b6f2cc2478451c8d4cca171852a8cb51f09c232
l.hernandezn.2018 <l.hernandezn.2018@alumnos.urjc.es>
2023-04-20 10:30:25 +0200

    commit ej6

---
+++ P5/Ej-03/Ej-03.js
@@ -0,0 +1,54 @@
+console.log("Ejecutando JS....")
+
+//-- Obtener elementos del DOM
+const canvas = document.getElementById('canvas');
+const img = document.getElementById('imagesrc');
+const ctx = canvas.getContext('2d');
+
+
+//-- Función de retrollamada de imagen cargada
+//-- La imagen no se carga instantaneamente, sino que
+//-- lleva un tiempo. Sólo podemos acceder a ella una vez
+//-- que esté totalmente cargada
+img.onload = function () {
+
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavía
+  ctx.drawImage(img, 0,0);
+
+  console.log("Imagen lista...");
+};
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavía
+  ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+    //--Calcular el brillo para obtener la imagen en escala de grises
+    for (i = 0; i < data.length; i += 4) {
+      var r = data[i];
+      var g = data[i+1];
+      var b = data[i+2];
+      brillo = (3 * r + 4 * g + b) / 8;
+      data[i] = brillo;
+      data[i+1] = brillo;
+      data[i+2] = brillo;
+    }
+
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+
+console.log("Fin...");
+  
+  
+  
